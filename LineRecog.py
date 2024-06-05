5ebddff01a762c084184a8b608bc5a0086485c44
Your Name <you@example.com>
2024-06-05 13:37:38 +0200

    Changes to LineRecog.py

--- LineRecog.py
+++ LineRecog.py
@@ -2,12 +2,11 @@ import cv2
 import os
 import numpy as np
 
-# Function to detect lines in an image and draw them
+# Function to detect lines in an image and draw them on the image
 def detect_and_draw_lines(image_path, output_path):
-    # Read the image and convert to GreyScale. Also add Blur to mask the noise in the image
+    
     image = cv2.imread(image_path, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
 
     blur = cv2.GaussianBlur(gray, (5,5), 0)
 
@@ -16,7 +15,7 @@ def detect_and_draw_lines(image_path, output_path):
     lines = cv2.HoughLines(blurred_edges, 1, np.pi/360, 80)
 
 
-    # Draw the lines on the image
+    # Draw the lines on the image for HoughLines
     if lines is not None:
         for line in lines:
             rho, theta = line[0]
@@ -33,17 +32,66 @@ def detect_and_draw_lines(image_path, output_path):
     # Save the output image
     cv2.imwrite(output_path, image)
 
+
+
+def detect_and_draw_lines_HoughLinesP(image_path, output_path):
+    
+    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    #convert image to GrayScale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+
+    #Canny Edge Detection:
+
+    #cthreshold values 150-350 find lines only in modified image GefaltetesBild
+
+    cthreshold1 = 40
+    cthreshold2 = 100
+    filterSize = 5
+
+    blurred_edges = cv2.Canny(blur, cthreshold1, cthreshold2, filterSize)
+
+    rho = 1
+    theta = np.pi/180
+    threshold = 50
+    minLineLength = 50
+    maxLineGap = 80
+    
+    lines = cv2.HoughLinesP(blurred_edges, rho, theta, threshold, 
+                            minLineLength=minLineLength, maxLineGap=maxLineGap)
+
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2 = line[0]
+            cv2.line(image,(x1,y1),(x2,y2), (0,255,0),2)
+
+    # Save the output image
+    cv2.imwrite(output_path, image)
+
+
+output_folder = 'new_lined_output'
+input_folder = 'new_images'
+HoughLinesP = True
+
 # Create a directory for output images if it doesn't exist
-output_folder = 'lined_output'
+
+
+
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# Process each image in the 'output' folder
-input_folder = 'output'
 for filename in os.listdir(input_folder):
     if filename.endswith(".jpg"):
         image_path = os.path.join(input_folder, filename)
         output_path = os.path.join(output_folder, filename)
-        detect_and_draw_lines(image_path, output_path)
+        if HoughLinesP:
+            detect_and_draw_lines_HoughLinesP(image_path, output_path)
+        else: 
+            detect_and_draw_lines(image_path,output_path)
 
-print("Line detection and drawing complete.")
+print("Line detection and drawing complete. Using HoughLinesP: ", HoughLinesP)
+
+#TODO: Find a way to line the dimmer structures of the image
+#TODO: Cluster lines together to form one line using some Algorithm
+#TODO: Extract that lines coordinates
